c22ba43d5533edcab8804f7ef07ff4a8e93fbae9
Katungye Johnson <katungye@users.noreply.github.com>
2024-05-13 18:31:18 +0200

    Updated the front end
    Missing icon

--- public/js/script.js
+++ public/js/script.js
@@ -1,22 +1,27 @@
-//listen to the submit event on the form and send a POST request to the server wait for the response and then update the UI
+// script.js
 
-// get element from the DOM
-const batton = document.getElementById('generate');
+const getWeather = document.getElementById('getWeather');
 
+getWeather.addEventListener('click', function () {
+    // if button clicked, then send a request to the API
+    const url = '/Weather'; // Enclose the URL string in quotes
 
-//chech id the button is clicked
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayWeather(data))
+})
 
-batton.addEventListener('click', async () => {
-	const response = await fetch('/weather');
-	const data = await response.json();
-	console.log(data);
-	document.getElementById('temp').innerHTML = data.main.temp;
-	document.getElementById('humidity').innerHTML = data.main.humidity;
-	document.getElementById('pressure').innerHTML = data.main.pressure;
-	document.getElementById('wind').innerHTML = data.wind.speed;
-	document.getElementById('description').innerHTML = data.weather[0].description;
-	document.getElementById('city').innerHTML = data.name;
-	document.getElementById('country').innerHTML = data.sys.country;
-	//document.getElementById('icon').src = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
-	//document.getElementById('weather').style.display = 'block';
-});
+function displayWeather(data) {
+    const cityElement = document.getElementById('city');
+    const temperatureElement = document.getElementById('temperature');
+    const weatherElement = document.getElementById('weather');
+    const windSpeedElement = document.getElementById('windSpeed');
+    const humidityElement = document.getElementById('humidity');
+
+    cityElement.textContent = data.name;
+    const temperatureCelsius = (data.main.temp - 273.15).toFixed(2); // Convert temperature to Celsius and round to 2 decimal places
+    temperatureElement.textContent = `${temperatureCelsius} °C`;
+    weatherElement.textContent = data.weather[0].description;
+    windSpeedElement.textContent = `${data.wind.speed} m/s`;
+    humidityElement.textContent = `${data.main.humidity}%`;
+}
